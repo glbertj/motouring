7cc5a84f3bc76e72b338b34427a631f9d843d6b2
Gilbert <gilbertjeff05@gmail.com>
2026-07-09 07:09:12 +0000

    feat: add manual AppContainer for repository DI

---
+++ app/src/main/java/com/valid/motouring/di/AppContainer.kt
@@ -0,0 +1,23 @@
+package com.valid.motouring.di
+
+import com.valid.motouring.data.repository.BadgeRepository
+import com.valid.motouring.data.repository.ChallengeRepository
+import com.valid.motouring.data.repository.NotificationRepository
+import com.valid.motouring.data.repository.PoiRepository
+import com.valid.motouring.data.repository.PostRepository
+import com.valid.motouring.data.repository.RideBuddyRepository
+import com.valid.motouring.data.repository.RideRepository
+import com.valid.motouring.data.repository.UserRepository
+import com.valid.motouring.data.repository.VehicleRepository
+
+class AppContainer {
+    val userRepository = UserRepository()
+    val vehicleRepository = VehicleRepository()
+    val rideBuddyRepository = RideBuddyRepository()
+    val rideRepository = RideRepository()
+    val challengeRepository = ChallengeRepository()
+    val badgeRepository = BadgeRepository()
+    val poiRepository = PoiRepository()
+    val postRepository = PostRepository()
+    val notificationRepository = NotificationRepository()
+}
