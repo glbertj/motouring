8fee4f46adb2ef545b1820033c3a50094fcacc74
Gilbert <gilbertjeff05@gmail.com>
2026-07-11 13:53:11 +0000

    feat: add category accent colors to theme

--- app/src/main/java/com/valid/motouring/ui/theme/Color.kt
+++ app/src/main/java/com/valid/motouring/ui/theme/Color.kt
@@ -13,6 +13,13 @@ val OffWhite = Color(0xFFF5F1EC)
 val Muted = Color(0xFFA89F97)
 val MutedDim = Color(0xFF7A8087)
 
+// Category accents (used by POI pins, group bar, tags). A restrained multi-hue set on top of the orange.
+val PoiFuel = Color(0xFF4ADE80)     // green
+val PoiRepair = AccentPrimary       // brand orange
+val PoiRest = Color(0xFFF5C34B)     // amber
+val RiderBlue = Color(0xFF7CB8FF)   // self / rider
+val SpeakingGreen = Color(0xFF4ADE80)
+
 object MotouringColors {
     val ringTrack = Charcoal600
     val ringProgress = AccentPrimary
@@ -21,4 +28,9 @@ object MotouringColors {
     val badgeLocked = Charcoal600
     val badgeEarned = AccentPrimary
     val liked = AccentPrimary
+    val poiFuel = PoiFuel
+    val poiRepair = PoiRepair
+    val poiRest = PoiRest
+    val rider = RiderBlue
+    val speaking = SpeakingGreen
 }
